db9f3f1f8d5c69e2ea2474381fce447c9e05a205
CaoKang <caokang@outlook.com>
2020-04-26 16:15:16 +0800

    完善查询单个设备信息

--- tests/singlequery.php
+++ tests/singlequery.php
@@ -8,7 +8,7 @@
  * Source: checkactivatedstat.php
  * Project: libiocm
  */
-require './autoload.php';
+require '../vendor/autoload.php';
 $config = require './config.php';
 $app = new Zeevin\Libiocm\Application($config);
 $iotConfig = $app['config']->get('iot');
@@ -16,11 +16,12 @@ $cacheConfig = $app['config']->get('cache');
 
 $request = new \Zeevin\Libiocm\Dm\RequestAttribute\Devices\SingleQuery\Request();
 $request->setAppId($iotConfig['appId'])->setSelect('imsi');
-/** @var \Zeevin\Libiocm\Dm\SingleQueryClient $single ] */
-$single = $app['dm.singleQuery'];
+
 $device_id = '8e7b94d1-c38e-414f-81f2-54ce98ac774c';
-/** @var \Zeevin\Libiocm\Dm\RequestAttribute\Devices\SingleQuery\Request $ret */
-$ret = $single->setUrlExtend($device_id)->setUrlParams(
+$ret = $app->dmSingleQuery->setUrlExtend($device_id)->setUrlParams(
     $request->serialize('form-url-encode')
 )->request()->getResult();
+
 print_r($ret);
+print_r($ret->getStatusCode());
+print_r($ret->getDeviceId());
