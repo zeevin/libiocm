2194563d93b27fea1243d06cf95352c906a8aeea
CaoKang <caokang@outlook.com>
2020-04-26 17:47:30 +0800

    修改测试参数

--- tests/querydevices.php
+++ tests/querydevices.php
@@ -6,7 +6,7 @@ $app = new Zeevin\Libiocm\Application($config);
 $iotConfig = $app['config']->get('iot');
 
 $request = new Zeevin\Libiocm\Dm\RequestAttribute\Devices\QueryDevices\Request();
-$request->setAppId($iotConfig['appId'])->setPageNo(1);
+$request->setAppId($iotConfig['appId'])->setPageNo(0);
 
 $ret = $app->dmQueryDevices->setUrlParams($request->serialize('form-url-encode'))->request()->getResult();
 print_r($ret);
